e25248ccc10d6e497e49aed35f93faf2abcff6a7
romina793 <rominapozzuto@gmail.com>
2019-02-20 18:15:46 -0300

    GET para el detalle

--- app/src/main/java/com/example/romina/petapp/model/dao/MascotaService.java
+++ app/src/main/java/com/example/romina/petapp/model/dao/MascotaService.java
@@ -1,10 +1,12 @@
 package com.example.romina.petapp.model.dao;
 
         import com.example.romina.petapp.model.pojo.ContainerMascota;
+        import com.example.romina.petapp.model.pojo.Mascota;
 
         import retrofit2.Call;
         import retrofit2.http.GET;
         import retrofit2.http.Headers;
+        import retrofit2.http.Path;
 
 public interface MascotaService {
 
@@ -13,4 +15,12 @@ public interface MascotaService {
     //Cada Call va a recibir un resultado del tipo especificado entre <>
     @Headers("Content-Type: application/json")
     Call<ContainerMascota> getMascotas();
+
+
+    // todo ----> agregar al pedido     @Headers("Content-Type: application/json")
+    @GET ("pet/{id]")
+    Call <Mascota> getMascotas (String data, @Path("id") String idMascota );
+
+
+
 }
