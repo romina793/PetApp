27ad051406413a62c2bf882101ceaf07dd4212ab
romina793 <rominapozzuto@gmail.com>
2019-02-20 12:49:08 -0300

    Atributos correctos del pojo

--- app/src/main/java/com/example/romina/petapp/model/pojo/Mascota.java
+++ app/src/main/java/com/example/romina/petapp/model/pojo/Mascota.java
@@ -1,33 +1,71 @@
 package com.example.romina.petapp.model.pojo;
 
+import android.nfc.Tag;
+
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
+import java.util.List;
 
 public class Mascota   implements  Serializable{
-/*
-    @SerializedName("name") private String nombreMascota;
-    @SerializedName("photoUrls") private Integer imagenMascota*/;
 
     private String name;
-    private String url;
+    private String id;
+    @SerializedName("photoUrls")
+    @Expose
+    private List<String> photoUrls = null;
+    @SerializedName("tags")
+    @Expose
+    private List<Tag> tags = null;
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+
 
     public Mascota() {
     }
 
-   /* public Mascota(String nombreMascota, Integer imagenMascota) {
-        this.nombreMascota = nombreMascota;
-        this.imagenMascota = imagenMascota;
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    }*/
+    public void setPhotoUrls(List<String> photoUrls) {
+        this.photoUrls = photoUrls;
+    }
+
+    public void setTags(List<Tag> tags) {
+        this.tags = tags;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<String> getPhotoUrls() {
+        return photoUrls;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public String getStatus() {
+        return status;
+    }
 
     public void setName(String name) {
         this.name = name;
     }
 
     public void setUrl(String url) {
-        this.url = url;
+        this.id = url;
     }
 
     public String getName() {
@@ -35,30 +73,9 @@ public class Mascota   implements  Serializable{
     }
 
     public String getUrl() {
-        return url;
+        return id;
     }
 
-
-
-/*
-
-    public String getNombreMascota() {
-        return nombreMascota;
-    }
-
-    public Integer getImagenMascota() {
-        return imagenMascota;
-    }
-
-
-    public void setNombreMascota(String nombreMascota) {
-        this.nombreMascota = nombreMascota;
-    }
-
-    public void setImagenMascota(Integer imagenMascota) {
-        this.imagenMascota = imagenMascota;
-    }
-*/
 
 
 }
