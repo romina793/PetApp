247ed01ea84cab50ffce587be8a0a9729d64541e
romina793 <rominapozzuto@gmail.com>
2019-02-20 10:53:20 -0300

    diseño xml y google maps

--- app/src/main/java/com/example/romina/petapp/view/MainActivity.java
+++ app/src/main/java/com/example/romina/petapp/view/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements AdapterMascota.Li
     private AdapterMascota adapter;
     private ProgressBar progressBar;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,8 +71,6 @@ public class MainActivity extends AppCompatActivity implements AdapterMascota.Li
             }
         });
 
-
-
     }
 
     @Override
@@ -79,4 +81,6 @@ public class MainActivity extends AppCompatActivity implements AdapterMascota.Li
         unIntent.putExtras(bundle);
         startActivity(unIntent);
     }
+
+
 }
